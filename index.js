f4431c85ff707efd2512c04a82c254d80e83c4ca
Juho Mäkinen <juho.makinen@applifier.com>
2014-09-22 09:00:43 +0300

    Add cmd line arguments

--- index.js
+++ index.js
@@ -2,19 +2,44 @@ var thrift = require('thrift');
 var scribe = require('./gen-nodejs/scribe');
 var ttypes = require('./gen-nodejs/scribe_types');
 var kafka = require('kafka-node');
+var optimist = require('optimist')
+  .usage('Start listening on scribed tcp feed and sends it to kafka found with --zookeepers argument')
+  .demand(['zookeepers'])
+  .default('port', 1463)
+  .default('statsd-port', 8125)
+  .default('statsd-prefix', "scribe2kafka")
+  .default('retries', 10)
+  .default('client-id', "scribe2kafka")
+  .default('verbose', false)
+  .describe('zookeepers', 'Zookeepers connection string')
+  .describe('port', 'Port where to listen for incoming scribed tcp messages')
+  .describe('retries', 'Kafka retry n times')
+  .describe('client-id', 'Kafka client-id')  
+  .describe('statsd-host', 'Optional statsd host where to send statsd statistics via udp')
+  .describe('statsd-port', 'port of statsd server')
+  .describe('statsd-prefix', 'statsd prefix')
+  .describe('verbose', 'verbose output (only for debugging)')
 
-var connectionString = "zk-1:2181"
+var argv = optimist.argv;
 
-var client = new kafka.Client(connectionString, "scribe2kafka", { retries : 10 });
+var connectionString = argv.zookeepers;
+
+var client = new kafka.Client(connectionString, argv["client-id"], { retries : argv.retries });
 var producer = new kafka.HighLevelProducer(client);
 
 var statsdClient = new require('statsd-client');
-var statsd = (new statsdClient({host: "statsd.applifier.info", prefix: "scribe2kafka", port:8127}));
+var statsd = null;
+if (argv["statsd-host"]) {
+  statsd = (new statsdClient({host: argv["statsd-host"], prefix: argv["statsd-prefix"], port:argv["statsd-port"]}));
+}
 
 
 var createServer = exports.createServer = function() {
 var server = thrift.createServer(scribe, {
     Log: function(entry, callback) {
+      if (argv.verbose) {
+        console.log("Got scribed message:", entry);
+      }
       var topics = {};
       for (var i = 0; i < entry.length; i++) {
         if (topics[entry[i].category] == undefined) {
@@ -30,14 +55,22 @@ var server = thrift.createServer(scribe, {
           topic : 'scribe.' + t,
           messages : topics[t]
         });
-        statsd.increment('scribe.' + t, topics[t].length);
+        if (statsd) {
+          statsd.increment('scribe.' + t, topics[t].length);
+        }
       }
 
 
       producer.send(payloads, function (err, data) {
+        if (argv.verbose) {
+          console.log("Kafka responded:", data, "err:", err);
+        }
+
         if (err) {
           console.log("Kafka sent. err:", err, "data:", data);
-          statsd.increment('error-to.' + t);
+          if (statsd) {
+            statsd.increment('error-to.' + t);
+          }
         }
       })
 
@@ -48,11 +81,14 @@ var server = thrift.createServer(scribe, {
   return server;
 };
 
+if (argv.verbose) {
+  console.log("Trying to connect to kafka broker...");
+}
 
 producer.on('ready', function() {
   var server = createServer();
-  server.listen(1463);
-  console.log("Started in port 1463");
+  server.listen(argv.port);
+  console.log("Started in port", argv.port);
 
 });
 
